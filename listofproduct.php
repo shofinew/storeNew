09fa8e01a688294f658111ac53a48ec9204ed921
shofinew <shofiullasafayat@gmail.com>
2024-10-09 12:55:25 +0600

    this new effort

---
+++ listofproduct.php
@@ -0,0 +1,43 @@
+<?php
+$con = mysqli_connect("localhost","root","","store");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>list of category</title>
+</head>
+<body>
+      <?php
+      $sql = "SELECT * FROM product";
+      $exsql = mysqli_query($con, $sql);
+
+      
+      if($exsql){
+            echo "<table border='1'>
+            <tr>
+            <th>pname</th>
+            <th>category</th>
+            <th>pcode</th>
+            <th>Action</th>
+            <tr>";
+            while($row = mysqli_fetch_assoc($exsql)){
+            $pid = $row['ProductID'];
+            $pname = $row['ProductNAME'];
+            $pcate = $row['ProductCATE'];
+            $pcode = $row['ProductCODE'];
+            
+            echo "<tr>
+            <td>$pid</td>
+            <td>$pname</td>
+            <td>$pcate</td>
+            <td><a href='editproduct.php ? id=$pid'>Edit</td>
+            </tr>";
+      } 
+
+}
+echo "</table>";
+      ?>
+</body>
+</html>
